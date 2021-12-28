7830de79cb4c045f8b086c1806c94b298b07b513
Davide Usberti <72393528+UsboKirishima@users.noreply.github.com>
2021-12-28 17:50:35 +0100

    Added java swing template

---
+++ scripts/SwingTemplate/SwingTemplate.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SwingTemplate {
+
+    private static JTextArea textArea;
+    private static JButton button;
+
+    public static void main(String[] args) {
+
+        //Create a new JFrame with a title.
+        JFrame frame = new JFrame("Yurd Swing Template!");
+
+        //Application UI components
+        frame.setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        button = new JButton("Click me!");
+
+        //Application deault size.
+        frame.setSize(800, 500);
+
+        //Open the application in the center of the screen.
+        frame.setLocationRelativeTo(null);
+
+        //Lock the application resizable. (Replace 'false' in 'true' if you want to Unlock this function)
+        frame.setResizable(false);
+
+        //Close the applicatin when the cross is pressed.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set the application visible (Put this function after the application code).
+        frame.setVisible(true);
+    }
+
+}
