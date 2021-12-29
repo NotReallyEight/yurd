40806857e0ec5d49a32f3fb9dd6d5ff5200722f0
Davide Usberti <72393528+UsboKirishima@users.noreply.github.com>
2021-12-29 20:44:19 +0100

    Added Rust Cli Template.

---
+++ scripts/rustcli/main.rs
@@ -0,0 +1,18 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "demo")]
+struct Opt {
+    #[structopt(short = "g", long = "greet")]
+    pub greet: bool,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    if opt.greet {
+        println!("Hello, welcome to Yurd cli template!");
+    }
+    else {
+        println!("missing something?");
+    }
+}
