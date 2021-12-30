389801cb1444f439ecc77eb92c764c12605119de
Thebigbot0000 <77632836+Thebigbot0000@users.noreply.github.com>
2021-12-30 08:27:53 +0100

    Fix err

--- template/discordext.js
+++ template/discordext.js
@@ -3,9 +3,21 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='>')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.event
+async def on_ready():
+    print('Ready!')
+
+# Command Example
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send('Pong!')
+#
+# or
+#
+# @bot.event
+#async def on_message(message):
+#    if message.content == "pong":
+#        await message.channel.send('ping')
 
 bot.run('token')`
 
